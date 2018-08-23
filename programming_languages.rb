4cd85cc65e92b604f9cb2d471dbf2fdab2967e11
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-23 15:20:30 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,6 +6,7 @@ def reformat_languages(languages)
   languages.each do |lang,value|
     value.each do |lang_name, lang_type|
       lang_type.each do |x,y|
+        new_hash[lang_type]={"#{x}:" y}
         binding.pry
     end
   end
